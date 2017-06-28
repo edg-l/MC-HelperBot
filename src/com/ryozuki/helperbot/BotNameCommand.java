9baa49c8d3e0c3204d8d40b84ced413a26f1957a
Ryozuki <clud6565@gmail.com>
2017-06-29 01:21:54 +0200

    first commit

---
+++ src/com/ryozuki/helperbot/BotNameCommand.java
@@ -0,0 +1,27 @@
+package com.ryozuki.helperbot;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class BotNameCommand implements CommandExecutor {
+    Main main = null;
+
+    public  BotNameCommand(Main plugin) {
+        this.main = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(strings.length < 1) {
+            commandSender.sendMessage(ChatColor.RED + "Usage: /setbotname <name>");
+            return false;
+        }
+        String newName = String.join(" ", strings).replace("\"", "");
+        main.setBotName(newName);
+        commandSender.sendMessage(ChatColor.BOLD.toString() + ChatColor.GREEN + "Succesfully set the name to:" + newName);
+        return true;
+    }
+}
+
